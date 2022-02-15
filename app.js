b1dd7fb1cf6262479db9e0a6f3f46f29245d564a
Misash <misash@outlook.com>
2022-02-14 23:41:51 -0500

    end

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash")
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -12,37 +13,37 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
 const posts = []
 
 
-app.get("/",(req,res)=>{
-  res.render("home" , {startContent: homeStartingContent , posts: posts})
+app.get("/", (req, res) => {
+  res.render("home", { startContent: homeStartingContent, posts: posts })
 
 })
 
 
-app.get("/about",(req,res)=>{
-  res.render("about" , {aboutContent: aboutContent})
+app.get("/about", (req, res) => {
+  res.render("about", { aboutContent: aboutContent })
 })
 
-app.get("/contact",(req,res)=>{
-  res.render("contact" , {contactContent: contactContent})
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactContent: contactContent })
 })
 
-app.get("/compose",(req,res)=>{
-  res.render("compose" )
+app.get("/compose", (req, res) => {
+  res.render("compose")
 })
 
-app.post("/compose",(req,res)=>{
+app.post("/compose", (req, res) => {
 
 
   const post = {
-    title: req.body.postTitle ,
-    body: req.body.postContent 
+    title: req.body.postTitle,
+    body: req.body.postContent
   }
 
   posts.push(post)
@@ -54,26 +55,21 @@ app.post("/compose",(req,res)=>{
 app.get('/posts/:postName', function (req, res) {
 
 
-  let found = false 
+  for (let i = 0; i < posts.length; i++) {
+    if (_.lowerCase(posts[i].title) == _.lowerCase(req.params.postName)) {
+      console.log("match!")
+      res.render("post", { title: posts[i].title, body: posts[i].body })
 
-  for(var i=0; i< posts.length ; i++){
-    if(posts[i].title == req.params.postName){
-      found = true 
-      break
+      return
     }
   }
 
-  if(found){
-    console.log("match!")
-  }else{
-    console.log("no found")
-  }
-
+  console.log("no found")
 
 })
 
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
